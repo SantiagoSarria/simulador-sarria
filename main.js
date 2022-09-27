fab94e44e8833343025258e09ff974d1dcedfe71
Santiago Sarria <santisarria2009@gmail.com>
2022-09-26 23:35:16 -0300

    Creando repositorio

---
+++ main.js
@@ -0,0 +1,8 @@
+edad = 12;
+if (edad >= 11) {
+  alert("No puedes jugar en la plaza");
+} else if (edad <= 12) {
+  alert("Puedes jugar a cualquier juego de la plaza");
+} else if (edad < 18) {
+  alert("Estas grandesito para jugar huevon");
+}
