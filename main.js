f00f1f7288f10fc137c6d68e674953706dea210c
Santiago Sarria <santisarria2009@gmail.com>
2022-09-30 10:28:59 -0300

    Cambios esteticos y javascript

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-edad = 12;
-if (edad >= 11) {
-  alert("No puedes jugar en la plaza");
-} else if (edad <= 12) {
-  alert("Puedes jugar a cualquier juego de la plaza");
-} else if (edad < 18) {
-  alert("Estas grandesito para jugar huevon");
+alert("Ingrese la opcion del producto que desea llevar (Precios x kg)")
+let seleccionarProductos = Number(prompt( "1-Ojo de bife 1kg $1700 2-Bife de chorizo 1kg $1750 3-Entraña 1kg $2000 4-Cabeza de lomo 1kg $1900"))
+let seleccionarCantidad;
+let total = 0;
+
+
+const cantidad = (cant, precio) => {
+  return cant * precio
+}
+
+
+while (seleccionarProductos != 0) {
+  switch (seleccionarProductos) {
+    case 1:
+      seleccionarCantidad= Number(prompt("El producto seleccionado es Ojo de bife, indique la cantidad de kg"))
+            total += cantidad(seleccionarCantidad, 1700)
+      break;
+      case 2:
+        seleccionarCantidad = Number(prompt("El producto seleccionado es Bife de chorizo, indique la cantidad de kg"))
+        total += cantidad(seleccionarCantidad, 1750)
+      break;
+    case 3:
+      seleccionarCantidad = Number(prompt("El producto seleccionado es Entraña, indique la cantidad de kg"))
+      total += cantidad(seleccionarCantidad, 2000)
+    break;
+    case 4:
+      seleccionarCantidad = Number(prompt("El producto seleccionado es Cabeza de lomo, indique la cantidad de kg"))
+      total += cantidad(seleccionarCantidad, 1900)
+    break;
+
+    default:
+      break;
+  }
+  seleccionarProductos = Number(prompt( "1-Ojo de bife $1700 2-Bife de chorizo $1750 3-Entraña $2000 4-Cabeza de lomo $1900 de kg"))
+}
+
+alert("El total de la compra es de: " + total)
+
+
+const envio = () => {
+    if (total >= 10000) {
+      alert("El envio es gratuito")
+    }else{
+      total += 1000
+      alert("El costo de envio es de 1000, el total es: " + total)
+    }
 }
